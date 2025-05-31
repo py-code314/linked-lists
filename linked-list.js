51c1a5448a470caa15141731b1441af133477cfa
srikk <122169588+py-code314@users.noreply.github.com>
2025-05-30 20:16:10 -0400

    feat: Add append() function

---
+++ linked-list.js
@@ -0,0 +1,46 @@
+import Node from './node.js'
+
+// const n1 = new Node(40)
+// console.log(n1)
+
+// Create a class to add, remove nodes to a linked list
+export default class LinkedList {
+  constructor() {
+    this.size = 0
+    this.head = null
+    // this.tail = null
+  }
+
+  // Append a node to the end of list
+  append(value) {
+    const node = new Node(value)
+
+    if (!this.head) {
+      this.head = node
+    } else {
+      let currentNode = this.head
+      while (currentNode.nextNode) {
+        currentNode = currentNode.nextNode
+      }
+      currentNode.nextNode = node
+    }
+
+    this.size++
+  }
+
+
+
+  // Print all values in the list in a specific format
+  toString() {
+    let linkedListString = ''
+    let currentNode = this.head
+
+    while (currentNode) {
+      let currentValue = currentNode.value.toString()
+      linkedListString += ` (${currentValue}) ->`
+      currentNode = currentNode.nextNode
+    }
+
+    console.log(linkedListString + ' null')
+  }
+}
---
+++ main.js
@@ -0,0 +1,12 @@
+import LinkedList from "./linked-list.js";
+
+const ll = new LinkedList()
+ll.append(10)
+// ll.append('dog')
+ll.prepend(20)
+ll.prepend(30)
+// ll.append(40)
+
+console.dir(ll, {depth: null})
+// ll.toString()
+
